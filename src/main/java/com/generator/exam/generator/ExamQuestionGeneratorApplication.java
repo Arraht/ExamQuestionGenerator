3340eef4702053620595e033510397619ae003d0
Arraht <igor_161995@mail.ru>
2024-05-07 13:43:37 +0600

    initial commit

---
+++ src/main/java/com/generator/exam/generator/ExamQuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.generator.exam.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generator/exam/generator/ExamQuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generator.exam.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
